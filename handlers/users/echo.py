677776232179113e2626cb81d109ead2724d060e
Grosvold <grosvold.ru@gmail.com>
2022-01-28 08:02:32 +0300

    Update echo.py

--- handlers/users/echo.py
+++ handlers/users/echo.py
@@ -7,7 +7,7 @@ from loader import dp
 # Эхо хендлер, куда летят текстовые сообщения без указанного состояния
 @dp.message_handler(state=None)
 async def bot_echo(message: types.Message):
-    await message.answer(f"Эхо без состояния."
+    await message.answer(f"Эхо без состояния. "
                          f"Сообщение:\n"
                          f"{message.text}")
 
