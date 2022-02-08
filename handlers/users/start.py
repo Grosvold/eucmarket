94d71ae63d165c1ba79451e69963437fb71aac90
Grosvold <grosvold.ru@gmail.com>
2022-02-08 16:25:23 +0300

    Update start.py

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -1,3 +1,5 @@
+import re
+
 from aiogram import types
 from aiogram.dispatcher.filters.builtin import CommandStart
 
@@ -5,11 +7,23 @@ from filters import IsPrivate
 from data.config import channel_name, ADMINS
 from loader import dp
 
+@dp.message_handler(IsPrivate(), user_id=ADMINS, text="/start")
+async def admin_chat_secret(message: types.Message):
+    await message.answer(f"Привет, {message.from_user.full_name}! ! \nЯ бот для {channel_name}.\n"
+                         f"Ты в списке модераторов, цели модератора здесь: https://t.me/c/1163833793/2 \n"
+                         f"Предлагаю ознакомиться с правилами и подать объявление.")
+
+
+@dp.message_handler(CommandStart(deep_link=re.compile(r"\d\d\d")))
+async def bot_start(message: types.Message):
+    deep_links_args = message.get_args()
+    await message.answer(f"Привет, {message.from_user.full_name}! ! \nЯ бот для {channel_name}.\n"
+                         f"Ты пришел по приглашению {deep_links_args}\n"
+                         f"Предлагаю ознакомиться с правилами и подать объявление.")
+
 
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
     await message.answer(f"Привет, {message.from_user.full_name}! ! \nЯ бот для {channel_name}.\n"
-                         f"Предлагаю ознакомиться с правилами и подать объявление."
-        # reply_markup=ReplyKeyboardMarkup(reply_keyboard, resize_keyboard=True, one_time_keyboard=False),
-    )
+                         f"Предлагаю ознакомиться с правилами и подать объявление.")
 
