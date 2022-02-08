c22d50e6f100754383d46fd3497c5504f1dc5f9c
Grosvold <grosvold.ru@gmail.com>
2022-02-08 20:17:59 +0300

    Update error_handler.py

--- handlers/errors/error_handler.py
+++ handlers/errors/error_handler.py
@@ -1,8 +1,4 @@
-import logging
-from aiogram.utils.exceptions import (TelegramAPIError,
-                                      MessageNotModified,
-                                      CantParseEntities)
-
+from aiogram.types import Update
 
 from loader import dp
 
@@ -16,22 +12,50 @@ async def errors_handler(update, exception):
     :param exception:
     :return: stdout logging
     """
+    from aiogram.utils.exceptions import (Unauthorized, InvalidQueryID, TelegramAPIError,
+                                          CantDemoteChatCreator, MessageNotModified, MessageToDeleteNotFound,
+                                          MessageTextIsEmpty, RetryAfter,
+                                          CantParseEntities, MessageCantBeDeleted, BadRequest)
 
+    if isinstance(exception, CantDemoteChatCreator):
+        logging.debug("Can't demote chat creator")
+        return True
 
     if isinstance(exception, MessageNotModified):
-        logging.exception('Message is not modified')
-        # do something here?
+        logging.debug('Message is not modified')
         return True
-      
+    if isinstance(exception, MessageCantBeDeleted):
+        logging.info('Message cant be deleted')
+        return True
+
+    if isinstance(exception, MessageToDeleteNotFound):
+        logging.info('Message to delete not found')
+        return True
+
+    if isinstance(exception, MessageTextIsEmpty):
+        logging.debug('MessageTextIsEmpty')
+        return True
+
+    if isinstance(exception, Unauthorized):
+        logging.info(f'Unauthorized: {exception}')
+        return True
+
+    if isinstance(exception, InvalidQueryID):
+        logging.exception(f'InvalidQueryID: {exception} \nUpdate: {update}')
+        return True
+
     if isinstance(exception, CantParseEntities):
-        # or here
-        logging.exception(f'CantParseEntities: {exception} \nUpdate: {update}')
+        await Update.get_current().message.answer(f'Попало в эррор хендлер. CantParseEntities: {exception.args}')
         return True
-      
-    #  MUST BE THE  LAST CONDITION (ЭТО УСЛОВИЕ ВСЕГДА ДОЛЖНО БЫТЬ В КОНЦЕ)
+
+    if isinstance(exception, RetryAfter):
+        logging.exception(f'RetryAfter: {exception} \nUpdate: {update}')
+        return True
+    if isinstance(exception, BadRequest):
+        logging.exception(f'BadRequest: {exception} \nUpdate: {update}')
+        return True
     if isinstance(exception, TelegramAPIError):
         logging.exception(f'TelegramAPIError: {exception} \nUpdate: {update}')
         return True
-    
-    # At least you have tried.
+
     logging.exception(f'Update: {update} \n{exception}')
