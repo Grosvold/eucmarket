5f02e0d72858a754b68dece49de1e879d0598227
Grosvold <grosvold.ru@gmail.com>
2022-02-08 20:02:06 +0300

    Update start.py

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -13,7 +13,7 @@ async def bot_start(message: types.Message):
     deep_links_args = message.get_args()
     bot_user = await dp.bot.me
     deep_link = f"https://t.me/{bot_user.username}?start=12345"
-    await message.answer(f"Привет, {message.from_user.full_name}! ! \nЯ бот для {channel_name}.\n"
+    await message.answer(f"Привет, {message.from_user.full_name}! \nЯ бот для {channel_name}.\n"
                          f"{deep_links_args} передаёт тебе привет!\n"
                          f"Твоя диплинк ссылка - {deep_link}\n"
                          f"Предлагаю ознакомиться с правилами и подать объявление.")
@@ -22,13 +22,13 @@ async def bot_start(message: types.Message):
 # Специальное приветствие для админов
 @dp.message_handler(IsPrivate(), user_id=ADMINS, text="/start")
 async def admin_chat_secret(message: types.Message):
-    await message.answer(f"Привет, {message.from_user.full_name}! ! \nЯ бот для {channel_name}.\n"
+    await message.answer(f"Привет, {message.from_user.full_name}! \nЯ бот для {channel_name}.\n"
                          f"Ты в списке модераторов, цели модератора здесь: https://t.me/c/1163833793/2 \n"
                          f"Предлагаю ознакомиться с правилами и подать объявление.")
 
 
-# Стандартное приветствие
-@dp.message_handler(CommandStart())
+# Стандартное приветствие в личку
+@dp.message_handler(IsPrivate(), CommandStart())
 async def bot_start(message: types.Message):
     await message.answer(f"Привет, {message.from_user.full_name}! ! \nЯ бот для {channel_name}.\n"
                          f"Предлагаю ознакомиться с правилами и подать объявление.")
