9046b2e5ab393349898bec4f8189c6d94acfcc8b
Grosvold <grosvold.ru@gmail.com>
2022-02-08 17:20:14 +0300

    Update test.py

--- states/test.py
+++ states/test.py
@@ -1,6 +1,10 @@
 from aiogram.dispatcher.filters.state import StatesGroup, State
 
 
-class Test(StatesGroup):
+# Создаем группу состояний Test - для тестирования.
+# Если у вас другой смысл состояний - называйте соответственно, не надо тупо копировать Test
+class AdvertQA(StatesGroup):
+    # Создаем состояние в этой группе. Называйте каждое состояние соответственно его назначению.
+    # В данном случае Q1 - question 1, то есть первый вопрос. У вас это может быть по-другому
     Q1 = State()
     Q2 = State()
