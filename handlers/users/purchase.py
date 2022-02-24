57d6f53236136250c200a51348dd3c68d455c14a
Grosvold <grosvold.ru@gmail.com>
2022-02-24 15:24:44 +0300

    menu

--- handlers/users/purchase.py
+++ handlers/users/purchase.py
@@ -1,37 +1,61 @@
 import logging
 
+from typing import Union
+
 from aiogram.dispatcher.filters import Command
 from aiogram.types import Message, CallbackQuery
 
 from data.config import channel_name, ADMINS
 from keyboards.inline.callback_datas import buy_callback
-from keyboards.inline.choice_buttons import choice, pear_keyboard, apples_keyboard
+from keyboards.inline.choice_buttons import choice, about, onwheel_keyboard, apples_keyboard
 from loader import dp
 
 
 @dp.message_handler(Command("items"))
 async def show_items(message: Message):
     await message.answer(text=f'Привет {message.from_user.first_name}! \nЯ бот для {channel_name}.\n'
-        f'Предлагаю ознакомиться с правилами и подать объявление.\n\n',
-                         reply_markup=choice)
+                              f'Предлагаю ознакомиться с правилами и подать объявление.\n\n',
+                           reply_markup=choice)
 
 
 # Попробуйем отловить по встроенному фильтру, где в нашем call.data содержится "pear"
-@dp.callback_query_handler(text_contains="pear")
-async def buying_pear(call: CallbackQuery):
-    # Обязательно сразу сделать answer, чтобы убрать "часики" после нажатия на кнопку.
-    # Укажем cache_time, чтобы бот не получал какое-то время апдейты, тогда нижний код не будет выполняться.
-    await call.answer(cache_time=60)
+@dp.callback_query_handler(text_contains="onwheel")
+async def buying_onwheel(message: Union[Message, CallbackQuery]):
+    if isinstance(message, Message):
+        await message.answer(text=f'Привет', reply_markup=choice)
+    elif isinstance(message, CallbackQuery):
+        call = message
+        # await call.message.edit_text(text=f'Привет123', reply_markup=choice)
 
-    callback_data = call.data
+    #
+    # # Обязательно сразу сделать answer, чтобы убрать "часики" после нажатия на кнопку.
+    # # Укажем cache_time, чтобы бот не получал какое-то время апдейты, тогда нижний код не будет выполняться.
+    # # await call.answer(cache_time=60)
+    #     callback_data = call.data
+    #
+    # # Отобразим что у нас лежит в callback_data
+    # # logging.info(f"callback_data='{callback_data}'")
+    # # В питоне 3.8 можно так
+    #     logging.info(f"{callback_data=}")
 
-    # Отобразим что у нас лежит в callback_data
-    # logging.info(f"callback_data='{callback_data}'")
-    # В питоне 3.8 можно так
-    logging.info(f"{callback_data=}")
+        await call.message.edit_text(f'Привет {call.message.from_user.first_name}! \nЯ бот для {channel_name}. Версия 0.8.0\n'
+                              f'Создан и написан @grosvold для EUC сообщества.\n'
+                              f'Порядок на барахолке поддерживается этим ботом, и 15 модераторами.',
+                              reply_markup=about)
 
-    await call.message.answer("Вы выбрали купить грушу. Груша всего одна. Спасибо.",
-                              reply_markup=pear_keyboard)
+
+# Попробуйем отловить по встроенному фильтру, где в нашем call.data содержится "pear"
+@dp.callback_query_handler(text_contains="about")
+async def buying_onwheel(message: Union[Message, CallbackQuery]):
+    if isinstance(message, Message):
+        await message.answer(text=f'Привет', reply_markup=choice)
+    elif isinstance(message, CallbackQuery):
+        call = message
+
+        await call.message.edit_text(f'Привет111 {call.message.from_user.first_name}! \nЯ бот для {channel_name}. Версия 0.8.0\n'
+                              f'Создан и написан @grosvold для EUC сообщества.\n'
+                              f'Порядок на барахолке поддерживается этим ботом, и 15 модераторами.',
+                              reply_markup=choice)
 
 
 # Попробуем использовать фильтр от CallbackData
@@ -50,7 +74,7 @@ async def buying_apples(call: CallbackQuery, callback_data: dict):
 @dp.callback_query_handler(text="cancel")
 async def cancel_buying(call: CallbackQuery):
     # Ответим в окошке с уведомлением!
-    await call.answer("Вы отменили эту покупку!", show_alert=True)
+    await call.answer("Вы нажали Отмена!")#, show_alert=True)
 
     # Вариант 1 - Отправляем пустую клваиатуру изменяя сообщение, для того, чтобы ее убрать из сообщения!
     await call.message.edit_reply_markup(reply_markup=None)
--- keyboards/inline/choice_buttons.py
+++ keyboards/inline/choice_buttons.py
@@ -2,37 +2,72 @@ from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from keyboards.inline.callback_datas import buy_callback
 
 # Вариант 1, как в прошлом уроке
-# choice = InlineKeyboardMarkup(inline_keyboard=[
-#     [
-#         InlineKeyboardButton(text="Купить грушу", callback_data=buy_callback.new(item_name="pear")),
-#         InlineKeyboardButton(text="Купить яблоки", callback_data="buy:apple")
-#     ],
-#     [
-#         InlineKeyboardButton(text="Отмена", callback_data="next")
-#     ]
-# ])
+about = InlineKeyboardMarkup(inline_keyboard=[
+    [
+        InlineKeyboardButton(text="О боте", callback_data="about"),
+        InlineKeyboardButton(text="Правила", url="https://t.me/EUC_market_RU/3")
+    ],
+    [
+        InlineKeyboardButton(text="Отмена", callback_data="cancel")
+    ]
+])
 
 
 # Вариант 2 - с помощью row_width и insert.
 choice = InlineKeyboardMarkup(row_width=2)
 
-buy_pear = InlineKeyboardButton(text="О боте", callback_data=buy_callback.new(item_name="pear", quantity=1))
-choice.insert(buy_pear)
+buy_onwheel = InlineKeyboardButton(text="О боте", callback_data=buy_callback.new(item_name="onwheel", quantity=1))
+choice.insert(buy_onwheel)
 
-buy_apples = InlineKeyboardButton(text="Правила", callback_data="buy:apple:5")
-choice.insert(buy_apples)
+buy_rules = InlineKeyboardButton(text="Правила", url="https://t.me/EUC_market_RU/3")
+choice.insert(buy_rules)
+#
+# buy_apples = InlineKeyboardButton(text="Правила", callback_data="buy:apple:5")
+# choice.insert(buy_apples)
 
-cancel_button = InlineKeyboardButton(text="Написать объявление", callback_data="cancel")
+new_ad = InlineKeyboardButton(text="Написать объявление", callback_data="newad")
+choice.insert(new_ad)
+
+back_button = InlineKeyboardButton(text="Назад", callback_data="back")
+choice.insert(back_button)
+
+cancel_button = InlineKeyboardButton(text="Отмена", callback_data="cancel")
 choice.insert(cancel_button)
 
+#
+# about = InlineKeyboardMarkup(row_width=2)
+#
+# buy_onwheel = InlineKeyboardButton(text="О боте", callback_data=buy_callback.new(item_name="onwheel", quantity=1))
+# choice.insert(buy_onwheel)
+#
+# buy_rules = InlineKeyboardButton(text="Правила", url="https://t.me/EUC_market_RU/3")
+# choice.insert(buy_rules)
+#
+# new_ad = InlineKeyboardButton(text="Написать объявление", callback_data="newad")
+# choice.insert(new_ad)
+#
+# back_button = InlineKeyboardButton(text="Назад", callback_data="back")
+# choice.insert(back_button)
+#
+# cancel_button = InlineKeyboardButton(text="Отмена", callback_data="cancel")
+# choice.insert(cancel_button)
+
+
 # А теперь клавиатуры со ссылками на товары
-pear_keyboard = InlineKeyboardMarkup(inline_keyboard=[
+rules_keyboard = InlineKeyboardMarkup(inline_keyboard=[
     [
-        InlineKeyboardButton(text="Купи тут", url="https://onwheel.ru")
+        InlineKeyboardButton(text="Правила  тут", url="https://t.me/EUC_market_RU/3")
     ]
 ])
+
+onwheel_keyboard = InlineKeyboardMarkup(inline_keyboard=[
+    [
+        InlineKeyboardButton(text="Купи тут", url="https://onwheel.com")
+    ]
+])
+
 apples_keyboard = InlineKeyboardMarkup(inline_keyboard=[
     [
-        InlineKeyboardButton(text="Купи тут", url="https://eucschool.com")
+        InlineKeyboardButton(text="Правила  тут", url="https://t.me/EUC_market_RU/3")
     ]
 ])
