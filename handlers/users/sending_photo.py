526594c0589ee4560f2f3c81bcc33005bd03ef9a
Grosvold <grosvold.ru@gmail.com>
2022-04-17 11:09:20 +0300

    Update sending_photo.py

--- handlers/users/sending_photo.py
+++ handlers/users/sending_photo.py
@@ -15,33 +15,40 @@ async def get_file_id_v(message: types.Message):
     await message.reply(message.video.file_id)
 
 
-@dp.message_handler(Command("get_cat"))
+@dp.message_handler(Command("get_photo"))
 async def send_cat(message: types.Message):
-    photo = "AgACAgIAAxkBAAICcV6jF5kAARvDMn99PQuVe9fBg-TKcAACQ64xG0WQGEm4F3v9dsbAAg7Hwg8ABAEAAwIAA3kAA9c_BgABGQQ"  # File id
+    photo = "AgACAgIAAxkBAAIKlWJbovMm5Z8RbeBHcTPgRKDb3M3IAALOuTEbvjbgSlW6efLz6FrqAQADAgADeQADJAQ"  # File id
     # photo = "https://www.meme-arsenal.com/memes/3f5777727d3f6e263b4edbee5bd15a1b.jpg" # URL
     # photo = InputFile(path_or_bytesio="photos/cat.jpg")  # Local file
     await bot.send_photo(chat_id=message.from_user.id,
                          photo=photo,
-                         caption="Вот тебе фото кота /more_cats")
+                         caption="Вот тебе фото кота /more_photos")
 
 
-@dp.message_handler(Command("more_cats"))
+@dp.message_handler(Command("more_photos"))
 async def more_cats(message: types.Message):
     # Создаем альбом
     album = types.MediaGroup()
 
     # Прикрепляем фото и видео
-    photo_file_id = "AgACAgIAAxkBAAICcV6jF5kAARvDMn99PQuVe9fBg-TKcAACQ64xG0WQGEm4F3v9dsbAAg7Hwg8ABAEAAwIAA3kAA9c_BgABGQQ"
+    # Максимум в альбоме 10 медиа. Максим 1 видео в аольбоме. Видео можно воткнуть в любое место.
+    photo_file_id = "AgACAgIAAxkBAAIKkWJbop37y9ie2MY0EH0mPy8kixnGAALLuTEbvjbgSlGk9PQgX5UIAQADAgADeQADJAQ"
     photo_url = "https://i.pinimg.com/originals/13/25/68/132568dbe8f3316aa56551dba527e7f7.jpg"
-    photo_bytes = InputFile("photos/cat.jpg")
-    video_file_id = "BAACAgIAAxkBAAICd16jHbPzMn9TCtF7dhv7xowgz4_ZAAI6BwACRZAYSXaxS0By-HwIGQQ"
-    album.attach_photo(
-        photo=photo_file_id,
-        caption="Прикольный котик")
-    album.attach_photo(photo=photo_url)
-    album.attach_photo(photo=photo_bytes)
+    # photo_bytes = InputFile("photos/cat.jpg")
+    video_file_id = "BAACAgIAAxkBAAIKM2JbnZZzuimxMHoDAhBPuJ4crHHaAAJREgAC8cmQSfYSjtAQ7VWSJAQ"
+    album.attach_photo(photo=photo_file_id)
+    album.attach_photo(photo=photo_file_id)
+    album.attach_photo(photo=photo_file_id)
+    album.attach_photo(photo=photo_file_id)
+    album.attach_photo(photo=photo_file_id)
+    # album.attach_photo(photo=photo_file_id, caption="Прикольный котик")
+    # album.attach_photo(photo=photo_url)
+    # album.attach_photo(photo=photo_bytes)
+    # album.attach_photo(video=video_file_id)
     album.attach_video(video=video_file_id,
                        caption="Видео где котик запрыгивает на кровать")
+    album.attach_photo(photo=photo_file_id)
+    album.attach_photo(photo=photo_file_id)
 
     # Отправляем
     # await bot.send_media_group(chat_id=message.from_user.id, media=album)
