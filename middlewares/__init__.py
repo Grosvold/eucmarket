4ec33fe6f322c35ab09e2e74c6b07badef504e0b
Grosvold <grosvold.ru@gmail.com>
2022-04-22 12:56:36 +0300

    Update __init__.py

--- middlewares/__init__.py
+++ middlewares/__init__.py
@@ -1,13 +1,13 @@
 from aiogram import Dispatcher
 
 from loader import dp
-from .throttling import ThrottlingMiddleware
+# from .throttling import ThrottlingMiddleware
 from .big_brother import BigBrother
 from .sentinel import Sentinel
 from .acl import ACLMiddleware
 
 if __name__ == "middlewares":
-    dp.middleware.setup(ThrottlingMiddleware())
+    # dp.middleware.setup(ThrottlingMiddleware())
     dp.middleware.setup(ACLMiddleware())
     dp.middleware.setup(Sentinel())
     dp.middleware.setup(BigBrother())
