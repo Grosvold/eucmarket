51549872f1ac0582b7d8aaf7126bacf5dd638013
Grosvold <grosvold.ru@gmail.com>
2022-06-25 08:11:31 +0300

    Update set_bot_commands.py

--- utils/set_bot_commands.py
+++ utils/set_bot_commands.py
@@ -6,7 +6,7 @@ async def set_default_commands(dp):
         [
             types.BotCommand("start", "Запустить бота"),
             types.BotCommand("help", "Вывести справку"),
-            types.BotCommand("get_photo", "Прислать фото"),
-            types.BotCommand("more_photos", "Прислать больше фото"),
+            # types.BotCommand("get_photo", "Прислать фото"),
+            # types.BotCommand("more_photos", "Прислать больше фото"),
         ]
     )
