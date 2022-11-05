44a9d500f26407901e09d8aa99408972edd4f304
Grosvold <grosvold.ru@gmail.com>
2022-11-05 03:28:14 +0300

    Update choice_buttons.py

--- keyboards/inline/choice_buttons.py
+++ keyboards/inline/choice_buttons.py
@@ -1,4 +1,5 @@
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from data.config import channel_name
 from keyboards.inline.callback_datas import buy_callback
 
 about_button = InlineKeyboardButton(text="О боте", callback_data="about")
@@ -9,7 +10,8 @@ back_button = InlineKeyboardButton(text="В начало", callback_data="back")
 school_button = InlineKeyboardButton(text="Купи тут", url="https://eucschool.com")
 opps_button = InlineKeyboardButton(text="Возможности", callback_data="opps")
 helpus_button = InlineKeyboardButton(text="Помочь проекту", callback_data="helpus")
-channel_button = InlineKeyboardButton(text="Канал объявлениями", url="https://t.me/EUC_market_RU/3")
+channel_button = InlineKeyboardButton(text="Канал объявлениями", url="https://t.me/" + channel_name[1:] + "/3")
+# нарезка channel_name https://www.techiedelight.com/ru/remove-first-character-string-python/
 chat_button = InlineKeyboardButton(text="Чат с объявлениями", url="https://t.me/EUC_market/51")
 resurses_button = InlineKeyboardButton(text="Полный список ресурсов", callback_data="resurses")
 
